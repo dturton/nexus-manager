7868cb69db141b49fdd54496afd19e2dc0b3437b
David Turton <dturton@etrivo.com>
2022-05-29 08:49:31 -0400

    renamed durable

---
+++ src/Durable.ts
@@ -0,0 +1,39 @@
+import { JobExecution } from './entities';
+import Bree from 'bree';
+import * as path from 'node:path';
+
+Bree.extend(require('@breejs/ts-worker'));
+
+export default class Durable extends Bree {
+  constructor() {
+    super({
+      root: path.join(__dirname, 'jobs'),
+      jobs: ['worker1'],
+      removeCompleted: false,
+      outputWorkerMetadata: false,
+      defaultExtension: process.env.ENABLE_JS ? 'js' : 'ts',
+      errorHandler: (error, workerMetadata) => {
+        if (workerMetadata.threadId) {
+          console.info(
+            `There was an error while running a worker ${workerMetadata.name} with thread ID: ${workerMetadata.threadId}`,
+          );
+        } else {
+          console.info(
+            `There was an error while running a worker ${workerMetadata.name}`,
+          );
+        }
+
+        console.error(error);
+      },
+      workerMessageHandler: (message, workerMetadata) => {
+        console.info(message);
+      },
+    });
+  }
+
+  run1() {
+    this.run();
+  }
+}
+
+export { JobExecution };
